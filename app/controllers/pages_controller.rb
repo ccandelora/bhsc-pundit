7fd810d4d5cd4f69921fea1db51acf00e35bf29e
chris.candelora@americastestkitchen.com <chris.candelora@americastestkitchen.com>
2014-07-18 09:09:23 -0400

    fixed pages edit problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end
+
   def home
   end
 
@@ -29,4 +30,50 @@ class PagesController < ApplicationController
   def social
      @page = Page.find(3)
   end
+
+  # GET /pages/new
+  def new
+    @page = Page.new
+  end
+
+  # GET /pages/1/edit
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  # POST /pages
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to @page, notice: 'Page was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /pages/1
+  def update
+    if @page.update(page_params)
+      redirect_to @page, notice: 'Page was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /page/1
+  def destroy
+    @page.destroy
+    respond_to do |format|
+      format.html { redirect_to pages_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title, :text)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   end
 
   def randomized_background_image
-    images = ['BrettlLogo.jpg','bretts.jpg', 'fronthouse.jpg', 'hike_2.jpeg', 'mt_wash_from_bwoods.jpg', 'hike.jpeg', 'house.jpeg', 'mt_washington.jpg', 'summit_sign.jpg', 'tubing.jpg', 'tucks_2.jpeg', 'tucks_3.jpg', 'tucks.jpg', 'ullr.jpeg']
+    images = ['bretts.jpg', 'fronthouse.jpg', 'hike_2.jpeg', 'mt_wash_from_bwoods.jpg', 'hike.jpeg', 'house.jpeg', 'mt_washington.jpg', 'summit_sign.jpg', 'tubing.jpg', 'tucks_2.jpeg', 'tucks_3.jpg', 'tucks.jpg', 'ullr.jpeg']
     images[rand(images.size)]
   end
 
